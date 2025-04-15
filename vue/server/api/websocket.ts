ce23a7f8a88cdbd7aedd4650411cec294ed14d2b
Mashiyyat Delos Santos <devopsbo.midgamefrntenddev@it-americano.com>
2025-04-15 13:57:57 +0800

    chore: minor refacotoring

--- vue/server/api/websocket.ts
+++ vue/server/api/websocket.ts
@@ -1,14 +1,13 @@
-const roomID = 'chat-room'
 const messageHistory: any[] = []
 
 export default defineWebSocketHandler({
 	open(peer) {
-		peer.subscribe(roomID)
+		peer.subscribe('chat-room')
 	},
 	message(peer, message) {
 		messageHistory.push(JSON.parse(message.text()))
 
-		peer.publish(roomID, {
+		peer.publish('chat-room', {
 			...JSON.parse(message.text()),
 			messageHistory,
 		})
